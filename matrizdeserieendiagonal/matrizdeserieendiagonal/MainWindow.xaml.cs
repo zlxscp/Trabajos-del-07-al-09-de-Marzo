7cf1cbe44831113bc86425beddc67deeeb341051
Sergio Contreras Palomera <112669671+zlxscp@users.noreply.github.com>
2023-03-10 16:41:01 -0600

    Add files via upload

---
+++ matrizdeserieendiagonal/matrizdeserieendiagonal/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace matrizdeserieendiagonal
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private int[,] sergio; //arreglo bidimensional
+        private void miGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            cargar();
+            mostrar();
+
+        }
+        private void cargar()
+        {
+            sergio = new int[6, 6];
+            for (int r = 0; r < 6; r++)
+            {
+                for (int c = 0
+                    ; c < 6; c++)
+                {
+                    if (r == c)
+                    {
+                        sergio[r, c] = 1;
+                    }
+                }
+            }
+        }
+        private void mostrar()
+        {
+            for (int r = 0; r < 6; r++)
+            {
+                for (int c = 0; c < 6; c++)
+                {
+                    Label mil = new Label();
+                    mil.Content = sergio[r, c];
+                    Grid.SetColumn(mil,c);
+                    Grid.SetRow(mil, r);
+                    miGrid.Children.Add(mil);
+                }
+            }
+        }
+    }
+}
